95e39cd7fe69fc418be7633a5108a3c367523752
Ryan G. Marcoux <rmarcoux1@gmail.com>
2020-09-22 20:14:44 -0400

    inital changes

---
+++ src/main/java/com/projects/irrigationcontroller/IrrigationControllerApplication.java
@@ -0,0 +1,13 @@
+package com.projects.irrigationcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrrigationControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrrigationControllerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/projects/irrigationcontroller/model/Zone.java
@@ -0,0 +1,17 @@
+package com.projects.irrigationcontroller.model;
+
+/**
+ * @author Ryan G. Marcoux
+ */
+public class Zone {
+
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
